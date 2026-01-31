6bb70b4f98f1552fb8d0553570a02425b39f351b
S-Rashy <sadiqrashidah@gmail.com>
2026-01-31 23:28:40 +0100

    Added Cart store

--- src/stores/product.js
+++ src/stores/product.js
@@ -3,10 +3,6 @@ import { ref } from 'vue'
 export const useProductStore = defineStore('product', () => {
   const products = ref([])
 
-  // const setProducts = (newProducts) => {
-  //   products.value = newProducts;
-  // };
-
   const fetchProducts = async () => {
     try {
       const response = await fetch('https://fakestoreapi.com/products')
