aeea7e7294078c0acb6a799bdde70c2c7258fd03
S-Rashy <sadiqrashidah@gmail.com>
2026-02-06 23:49:53 +0100

    new updates added

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes.js'
-import { useAuthStore } from '@/stores/auth.js';
-
+import { useAuthStore } from '@/stores/auth.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
 router.beforeEach((to, from, next) => {
-
-  const authenticated = useAuthStore().isAuthenticated;
+  const authenticated = useAuthStore().isAuthenticated
 
   if (to.meta.requiresAuth && !authenticated) {
-    next({ name: "AuthPage" });
-    return;
+    next({ name: 'AuthPage' })
+    return
   }
 
   if (to.meta.requiresGuest && authenticated) {
-    next({ name: "HomePage" });
-    return;
+    next({ name: 'HomePage' })
+    return
   }
 
-  next();
-});
+  next()
+})
 
 export default router
