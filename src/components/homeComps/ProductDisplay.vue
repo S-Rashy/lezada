7f76482854a31f6f586f9229bcf00f219f11578c
S-Rashy <sadiqrashidah@gmail.com>
2026-02-15 23:18:01 +0100

    new update

--- src/components/homeComps/ProductDisplay.vue
+++ src/components/homeComps/ProductDisplay.vue
@@ -27,23 +27,17 @@ const displayedProducts = computed(() => {
     <div class="flex justify-center gap-10 my-12">
       <!-- @click="prodTab = 1" -->
       <!-- :class="prodTab === 1 ? 'text-[#333333]' : 'text-[#777]'" -->
-      <h2
-      class="text-[48px] hover:text-[#333333] cursor-pointer transition-colors duration-300"
-      >
+      <h2 class="text-[48px] hover:text-[#333333] cursor-pointer transition-colors duration-300">
         New
       </h2>
       <!-- @click="prodTab = 2" -->
       <!-- :class="prodTab === 2 ? 'text-[#333333]' : 'text-[#777]'" -->
-      <h2
-      class="text-[48px] hover:text-[#333333] cursor-pointer transition-colors duration-300"
-      >
+      <h2 class="text-[48px] hover:text-[#333333] cursor-pointer transition-colors duration-300">
         Popular
       </h2>
       <!-- @click="prodTab = 3" -->
       <!-- :class="prodTab === 3 ? 'text-[#333333]' : 'text-[#777]'" -->
-      <h2
-      class="text-[48px] hover:text-[#333333] cursor-pointer transition-colors duration-300"
-      >
+      <h2 class="text-[48px] hover:text-[#333333] cursor-pointer transition-colors duration-300">
         Sale
       </h2>
     </div>
