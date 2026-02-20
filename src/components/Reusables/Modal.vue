ba162b6a9eda501762a929daee6db7f1cbd81d81
S-Rashy <sadiqrashidah@gmail.com>
2026-02-20 23:08:13 +0100

    product details page

--- src/components/Reusables/Modal.vue
+++ src/components/Reusables/Modal.vue
@@ -1,34 +1,29 @@
 <script setup>
-import MainButton from './MainButton.vue';
+import MainButton from './MainButton.vue'
 
 const props = defineProps({
   to: {
     type: String,
-    default: "/"
-  }
-});
-const emit = defineEmits(["close-modal"]);
+    default: '/',
+  },
+})
+const emit = defineEmits(['close-modal'])
 const close = () => {
-  emit("close-modal");
-};
-
+  emit('close-modal')
+}
 </script>
 
 <template>
-  <div
-    class="w-full h-full inset-0 fixed flex items-center justify-center bg-black/30 z-100"
-  >
+  <div class="w-full h-full inset-0 fixed flex items-center justify-center bg-black/30 z-100">
     <div class="bg-white h-59 w-120 py-12 px-12 space-y-8 text-center">
       <h5><slot name="info"></slot></h5>
 
       <slot name="button">
         <!-- <RouterLink :to="to"> -->
-          <MainButton @click="close"><slot name="btn"></slot> </MainButton>
+        <MainButton @click="close"><slot name="btn"></slot> </MainButton>
         <!-- </RouterLink> -->
       </slot>
-      <slot>
-
-      </slot>
+      <slot> </slot>
     </div>
   </div>
 </template>
