c61980df3685dfb24dc940b00019c8fe16140f96
S-Rashy <sadiqrashidah@gmail.com>
2026-02-24 23:37:39 +0100

    product details page

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -1,17 +1,44 @@
-<script setup lang="ts">
-import { onMounted } from 'vue'
+<script setup>
+import { ref, computed, onMounted, watch } from 'vue'
 import { useProductStore } from '@/stores/product'
 
-import FilterSidebar from '@/components/shopComps/FilterSidebar.vue'
-import ProductGrid from '@/components/shopComps/ProductGrid.vue'
-import Topbar from '@/components/shopComps/Topbar.vue'
+// import FilterSidebar from '@/components/shopComps/FilterSidebar.vue'
+// import ProductGrid from '@/components/shopComps/ProductGrid.vue'
+// import Topbar from '@/components/shopComps/Topbar.vue'
 import PageHeader from '@/components/Reusables/PageHeader.vue'
+import ProductCard from '@/components/Reusables/ProductCard.vue'
 
 const productStore = useProductStore()
+const products = computed(() => productStore.getProducts)
+const currentPage = ref(1)
+const perPage = ref(10)
+const lastPage = computed(() => productStore.getPagination.last_page)
 
-onMounted(() => {
-  productStore.fetchProducts()
+onMounted(async () => {
+  await productStore.fetchProducts({
+    per_page: perPage.value,
+    page: currentPage.value,
+  })
 })
+
+watch(currentPage, (val) => {
+  productStore.fetchProducts({
+    page: val,
+    per_page: perPage.value,
+  })
+})
+
+const previous = () => {
+  if (currentPage.value > 1) {
+    currentPage.value--
+  }
+}
+const next = () => {
+  if (currentPage.value < lastPage.value) {
+    currentPage.value++
+  }
+}
+
 </script>
 
 <template>
@@ -20,18 +47,48 @@ onMounted(() => {
     <template #page>Shop</template>
   </PageHeader>
   <div class="max-w-7xl mx-auto px-6 py-8">
-    <Topbar />
+    <!-- <Topbar /> -->
 
-    <div class="grid grid-cols-12 gap-8 mt-8">
+   <div class="flex items-center justify-between mt-4">
+  <h3>
+    Showing {{ products.length }} of
+    {{ productStore.getPagination?.total || 0 }}
+  </h3>
+
+  <select v-model="perPage">
+    <option :value="10">10</option>
+    <option :value="15">15</option>
+    <option :value="20">20</option>
+    <option :value="25">25</option>
+  </select>
+</div>
+
+
+    <div class="grid grid-cols-3 gap-8 mt-8">
+      <ProductCard v-for="product in products" :key="product.id" :product="product" />
+
       <!-- Filters -->
-      <aside class="col-span-12 lg:col-span-3">
+      <!-- <aside class="col-span-12 lg:col-span-3">
         <FilterSidebar />
-      </aside>
+      </aside> -->
 
       <!-- Products -->
-      <section class="col-span-12 lg:col-span-9">
+      <!-- <section class="col-span-12 lg:col-span-9">
         <ProductGrid />
-      </section>
+      </section> -->
     </div>
+  </div>
+
+  <div class="mt-6 flex gap-3">
+    <button @click="previous">prev</button>
+    <button
+      v-for="page in lastPage"
+      :key="page"
+      @click="currentPage = page"
+      :class="currentPage === page ? 'underline' : 'bg-red'"
+    >
+      {{ page }}
+    </button>
+    <button @click="next">next</button>
   </div>
 </template>
--- src/stores/product.js
+++ src/stores/product.js
@@ -5,11 +5,12 @@ import api from '../helpers/axios'
 export const useProductStore = defineStore('product', () => {
   const products = ref([])
   const loading = ref(false)
+  const pagination = ref(null)
 
-  const minPrice = ref(null)
-  const maxPrice = ref(null)
-  const selectedCategory = ref(null)
-  const inStockOnly = ref(false)
+  // const minPrice = ref(null)
+  // const maxPrice = ref(null)
+  // const selectedCategory = ref(null)
+  // const inStockOnly = ref(false)
 
   const totalProducts = computed(() => products.value.length)
 
@@ -20,62 +21,71 @@ export const useProductStore = defineStore('product', () => {
       const response = await api.get('/products', { params })
       products.value = response.data.data
       console.log(products.value)
+      pagination.value = { ...response.data }
+      delete pagination.value.data
       loading.value = false
     } catch (error) {
       console.error('Error fetching products:', error)
     }
   }
 
-  const categories = computed(() => {
-    return [...new Set(products.value.map((p) => p.category))]
-  })
+  const getProducts = computed(() => products.value)
+  const getPagination = computed(() => pagination.value)
+  const getLoadingState = computed(() => loading.value)
 
-  const filteredProducts = computed(() => {
-    return products.value.filter((product) => {
-      if (minPrice.value !== null && product.price < minPrice.value) return false
-      if (maxPrice.value !== null && product.price > maxPrice.value) return false
-      if (selectedCategory.value && product.category !== selectedCategory.value) return false
-      if (inStockOnly.value && product.stock <= 0) return false
-      return true
-    })
+  
 
-  })
+  // const categories = computed(() => {
+  //   return [...new Set(products.value.map((p) => p.category))]
+  // })
 
-  async function fetchAllForPage() {
-  loading.value = true
+  // const filteredProducts = computed(() => {
+  //   return products.value.filter((product) => {
+  //     if (minPrice.value !== null && product.price < minPrice.value) return false
+  //     if (maxPrice.value !== null && product.price > maxPrice.value) return false
+  //     if (selectedCategory.value && product.category !== selectedCategory.value) return false
+  //     if (inStockOnly.value && product.stock <= 0) return false
+  //     return true
+  //   })
 
-  try {
-      const response = await api.get('/products', { 
-      params: {
-        page: 1,
-        per_page: 1000, 
-        category: selectedCategory.value,
-        min_price: minPrice.value || undefined,
-        max_price: maxPrice.value || undefined,
-        in_stock: inStockOnly.value || undefined
-      }
-    })
+  // })
 
-    products.value = response.data.data
-  } catch (err) {
-    console.error(err)
-  } finally {
-    loading.value = false
-  }
-}
+  // async function fetchAllForPage() {
+  //   loading.value = true
 
+  //   try {
+  //     const response = await api.get('/products', {
+  //       params: {
+  //         page: 1,
+  //         per_page: 1000,
+  //         category: selectedCategory.value,
+  //         min_price: minPrice.value || undefined,
+  //         max_price: maxPrice.value || undefined,
+  //         in_stock: inStockOnly.value || undefined,
+  //       },
+  //     })
+
+  //     products.value = response.data.data
+  //   } catch (err) {
+  //     console.error(err)
+  //   } finally {
+  //     loading.value = false
+  //   }
+  // }
 
   return {
     products,
     fetchProducts,
     totalProducts,
     loading,
-    categories,
-    filteredProducts,
-    fetchAllForPage,
-    minPrice,
-    maxPrice,
-    selectedCategory,
-    inStockOnly,
+    getPagination,
+    getProducts,
+    // categories,
+    // filteredProducts,
+    // fetchAllForPage,
+    // minPrice,
+    // maxPrice,
+    // selectedCategory,
+    // inStockOnly,
   }
 })
