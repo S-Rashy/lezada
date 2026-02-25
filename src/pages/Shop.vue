95f09b24276b12bbf16c6999c89b22829fcced92
S-Rashy <sadiqrashidah@gmail.com>
2026-02-25 23:07:05 +0100

    product details page

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -23,8 +23,8 @@ onMounted(async () => {
 
 watch([currentPage, perPage], () => {
   productStore.fetchProducts({
-    page: currentPage.value,
     per_page: perPage.value,
+    page: currentPage.value,
   })
 })
 
@@ -48,21 +48,25 @@ const next = () => {
   <div class="max-w-7xl mx-auto px-6 py-8">
     <!-- <Topbar /> -->
 
-    <div class="flex items-center justify-between mt-4">
+    <div class="flex items-center justify-center gap-24 mt-4">
       <h3>
         Showing
         {{ (currentPage - 1) * perPage + 1 }}
         –
         {{ (currentPage - 1) * perPage + products.length }}
-        of {{ productStore.getPagination?.total || 0 }}
+        of {{ productStore.getPagination?.total || 0 }} result
       </h3>
 
-      <select v-model="perPage">
-        <option :value="10">10</option>
-        <option :value="15">15</option>
-        <option :value="20">20</option>
-        <option :value="25">25</option>
-      </select>
+      <div class="space-x-2">
+        <span>Show</span>
+        <select v-model="perPage" class="border p-2">
+          <option :value="10">10</option>
+          <option :value="15">15</option>
+          <option :value="20">20</option>
+          <option :value="25">25</option>
+        </select>
+        <span>per page</span>
+      </div>
     </div>
 
     <div class="grid grid-cols-3 gap-8 mt-8">
@@ -80,13 +84,14 @@ const next = () => {
     </div>
   </div>
 
-  <div class="mt-6 flex gap-3">
+  <div class="mt-6 flex gap-5 w-full justify-center">
     <button @click="previous">prev</button>
     <button
       v-for="page in lastPage"
       :key="page"
       @click="currentPage = page"
-      :class="currentPage === page ? 'underline' : 'bg-red'"
+      class="p-2 cursor-pointer border-b border-transparent hover:text-[#333333] hover:border-b-[#333333]"
+      :class="currentPage === page ? 'border-b text-[#333333]' : 'text-[#777777]'"
     >
       {{ page }}
     </button>
